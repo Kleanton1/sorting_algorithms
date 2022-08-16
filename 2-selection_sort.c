2bb88c8591afce777f071853ab64a113c8178829
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-08-16 11:57:21 +0000

    Selection-sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size)
 		if (i != min)
 		{
 			swap = array[i];
-			array[i] =array[min];
+			array[i] = array[min];
 			array[min] = swap;
 			print_array(array, size);
 		}
