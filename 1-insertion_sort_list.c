ac6b3826463adbbc1c8fbbe664ccd49e6595dc17
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-08-16 11:03:18 +0000

    insertion_sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -18,7 +18,7 @@ void insertion_sort_list(listint_t **list)
 
 	for (forw = (*list)->next; forw && forw->prev; forw = forw->next)
 	{
-		for(; forw && forw->prev && forw->n < forw->prev->n;
+		for (; forw && forw->prev && forw->n < forw->prev->n;
 		    forw = forw->prev)
 		{
 			tmp = forw->prev;
@@ -52,7 +52,7 @@ void swap(listint_t **head, listint_t *node1, listint_t *node2)
 	node1->prev = node2;
 	node1->next = next;
 	node2->prev = prev;
-	node2 ->next = node1;
+	node2->next = node1;
 	if (next)
 		next->prev = node1;
 }
